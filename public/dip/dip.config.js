61abaa819a0758f7cc8e48d43e0de25590e46cc4
ImperialisticScum <114625848+Imperialfr@users.noreply.github.com>
2023-02-02 09:19:53 -0800

    imp (Real)ease

--- public/dip/dip.config.js
+++ public/dip/dip.config.js
@@ -11,7 +11,7 @@ self.__DIP.config = {
     path: '/bare/',
   },
   tab: {
-    title: 'Francium',
+    title: 'Francium→',
     icon: 'https://google.com/favicon.ico',
     ua: 'Mozilla/5.0 (X11; CrOS x86_64 14388.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.107 Safari/537.36'
   }
