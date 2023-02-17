38e623dd7237a2e5a8955c1272baa2e39b22c0a2
ImperialisticScum <114625848+Imperialfr@users.noreply.github.com>
2023-02-17 08:55:42 -0800

    Imp (real)ease

--- public/sw.js
+++ public/sw.js
@@ -6,5 +6,5 @@ const sw2 = new DIPServiceWorker('./dip/dip.worker.js');
 
 self.addEventListener('fetch', event => {
   if (event.request.url.startsWith(location.origin+'/usedservice/fip/')) event.respondWith(sw2.fetch(event));
-  if (event.request.url.startsWith(location.origin+'/service/uv/')) event.respondWith(sw.fetch(event));
+  if (event.request.url.startsWith(location.origin+'/usedservice/uv/')) event.respondWith(sw.fetch(event));
 });
